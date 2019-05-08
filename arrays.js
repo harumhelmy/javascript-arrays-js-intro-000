c5501fdba9970adf45a2231171cfaedc502faa0c
Harum Helmy <harumendhah@gmail.com>
2019-05-08 16:55:18 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -39,7 +39,7 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   return array
 }
 
-function removeElementFromEndofArray(array) {
+function removeElementFromEndOfArray(array) {
   array.slice(0, array.length - 1);
   return array
 }
