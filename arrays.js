8e97c3fd08cb09415fe8c8b21ebc2253f80a3117
Harum Helmy <harumendhah@gmail.com>
2019-05-08 16:21:35 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -14,3 +14,8 @@ function addElementToEndOfArray(array, element) {
   array = [...array, element]
   return array
 }
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array;
+}
