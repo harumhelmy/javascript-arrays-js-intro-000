3a402af60654c7aa4a12d1e59ffaafc5153a7097
Harum Helmy <harumendhah@gmail.com>
2019-05-08 16:19:59 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,10 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element) {
+  array = [element, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element);
   return array;
 }
